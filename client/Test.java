ec372bda0773523f5337982f9fb36120488d33ce
Neo Parihar <142837101+neoParihar@users.noreply.github.com.>
2025-07-24 02:07:26 -0500

    Continued work on Test

--- client/Test.java
+++ client/Test.java
@@ -1,15 +1,33 @@
 package client;
 
 import java.net.Socket;
+
+import javax.swing.*;
+import java.awt.*;
+
 import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 public class Test {
   private Socket socket;
   private BufferedWriter bufferedWriter;
 
-  public Test(Socket socket, BufferedWriter bufferedWriter) {
+  public Test(Socket socket) throws IOException {
     this.socket = socket;
-    this.bufferedWriter = bufferedWriter;
+    this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+    JFrame frame = new JFrame("Test Panel");
+    JButton sendButton = new JButton("Send Test Request");
+    sendButton.addActionListener(e -> {
+      bufferedWriter.write("TEST_REQUEST");
+    });
+
+    frame.setLayout(new FlowLayout());
+    frame.add(sendButton);
+    frame.setSize(300, 100);
+    frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    frame.setVisible(true);
   }
 
 }
